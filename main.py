8f8204389678ea34e899fcab420cc60ee562c1f0
dianbo <dianbo@dianbo.dot>
2023-04-16 20:36:02 +0000

    funct and agrs definition

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 """
 general file read and write utility based on bfs
-modified comment
+
 """
+
+import os
+from collections import deque
+
+
+def bfs_dir(htransform, src_root_dir: str, dst_root_dir=None):
+    """
+    htrasform : represents the function used to manipulate files of the explored dirs (given the conditions set in
+    htrasnform itself). src_root_dir : targeted parent/root directory. dst_root_dir : optional if None it will be
+    assigned by default.
+    """
